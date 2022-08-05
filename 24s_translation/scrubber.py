82e9cc51acbdf49740618722fc8a3b062a7b9eb4
zhxu <aldo4611034@gmail.com>
2022-08-05 15:29:33 +0200

    测试

--- 24s_translation/scrubber.py
+++ 24s_translation/scrubber.py
@@ -74,66 +74,72 @@ def regroup_word_by_len(list):
         pass
 
 list_docs = ['enfant-f_accesoires.json','enfant-f_chaussures.json','enfant-f_offre.json','enfant-f_vetements.json','enfant-g_accesoirs.json','enfant-g_chaussures.json','enfant-g_offre.json','enfant-g_vetements.json','femme_accesoires.json','femme_beaute.json','femme_bijoux.json','femme_chaussures.json','femme_offre.json','femme_sac.json','femme_vetements.json','homme_accesoires.json','homme_beaute.json','homme_chaussures.json','homme_offre.json','homme_sac.json','homme_sport.json','homme_vetements.json','maison_arts.json','maison_deco.json','maison_linge.json','maison_luminaires.json','maison_mobilier.json','maison_offre.json']
-for data_original in list_docs:
-    # file_path = 'D:\\dict_FrToCn\\24s_translation\\items_collection\\items_collection\\' + data_original
-    file_path = '/root/dict_FrToCn/24s_translation/items_collection/items_collection/' + data_original
-    with open(file_path,encoding='utf-8') as data_site:
-        data = json.load(data_site)
-        data1 = []
-        for each in data:
-            col = []
-            col.append(each['name_fr'])
-            col.append(each['name_zh'])
-            data1.append(col)
+try:
 
-    for i in range(len(data1)):
-        resultat_nettoye = nettoyer(data1[i][0], data1[i][1])
+    for data_original in list_docs:
+        # file_path = 'D:\\dict_FrToCn\\24s_translation\\items_collection\\items_collection\\' + data_original
+        file_path = '/root/dict_FrToCn/24s_translation/items_collection/items_collection/' + data_original
+        with open(file_path,encoding='utf-8') as data_site:
+            data = json.load(data_site)
+            data1 = []
+            for each in data:
+                col = []
+                col.append(each['name_fr'])
+                col.append(each['name_zh'])
+                data1.append(col)
 
-        list_binome = []
-        for each in resultat_nettoye[1]: #将中文原文中的每一个词取出来
-            max_ratio = 0
-            synonym_ratio = 0
-            meilleur_trans = ''
-            length = len(resultat_nettoye[0])
-            binome = []
+        for i in range(len(data1)):
+            resultat_nettoye = nettoyer(data1[i][0], data1[i][1])
 
-            for i in range(length):
-                n = length-i
-                google_trans = translate(resultat_nettoye[0][:n])
-                radio = difflib.SequenceMatcher(None, each, google_trans).ratio()
+            list_binome = []
+            for each in resultat_nettoye[1]: #将中文原文中的每一个词取出来
+                max_ratio = 0
+                synonym_ratio = 0
+                meilleur_trans = ''
+                length = len(resultat_nettoye[0])
+                binome = []
 
-                if radio >= max_ratio :
-                    r = synonyms.compare(each, google_trans, seg=True)
-                    if r >= synonym_ratio:
-                        synonym_ratio = r
-                        meilleur_trans = each
-                        original_fr = resultat_nettoye[0][:n]
-                        max_ratio = radio
-            if max_ratio >= 0.0:
-                binome.append(meilleur_trans)
-                binome.append(original_fr)
-                list_binome.append(binome)
+                for i in range(length):
+                    n = length-i
+                    google_trans = translate(resultat_nettoye[0][:n])
+                    radio = difflib.SequenceMatcher(None, each, google_trans).ratio()
 
-        list1 = regroup_word_by_len(list_binome)
-        print(list1)
-        file_name = '/root/dict_FrToCn/24s_translation/items_collection/dict_' + data_original.split('.')[0] + '.csv'
-        # file_name = 'dict_' + data_original.split('.')[0] + '.csv'
+                    if radio >= max_ratio :
+                        r = synonyms.compare(each, google_trans, seg=True)
+                        if r >= synonym_ratio:
+                            synonym_ratio = r
+                            meilleur_trans = each
+                            original_fr = resultat_nettoye[0][:n]
+                            max_ratio = radio
+                if max_ratio >= 0.0:
+                    binome.append(meilleur_trans)
+                    binome.append(original_fr)
+                    list_binome.append(binome)
 
-        try:
+            list1 = regroup_word_by_len(list_binome)
+            print(list1)
+            file_name = '/root/dict_FrToCn/24s_translation/items_collection/dict_' + data_original.split('.')[0] + '.csv'
+            # file_name = 'dict_' + data_original.split('.')[0] + '.csv'
 
-            with open (file_name, "a", encoding='utf-8-sig') as csvfile:
-                writer = csv.writer(csvfile)
-                for each in list1:
-                    list2 = []
-                    list2.append(each[1])
-                    list2.append(each[0])
-                    writer.writerow(list2)
-        except Exception as err:
-            with open (file_name, "w", encoding='utf-8-sig') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerow(["word_fr", "word_cn"])
-                for each in list1:
-                    list2 = []
-                    list2.append(each[1])
-                    list2.append(each[0])
-                    writer.writerow(list2)
+            try:
+
+                with open (file_name, "a", encoding='utf-8-sig') as csvfile:
+                    writer = csv.writer(csvfile)
+                    for each in list1:
+                        list2 = []
+                        list2.append(each[1])
+                        list2.append(each[0])
+                        writer.writerow(list2)
+            except Exception as err:
+                with open (file_name, "w", encoding='utf-8-sig') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow(["word_fr", "word_cn"])
+                    for each in list1:
+                        list2 = []
+                        list2.append(each[1])
+                        list2.append(each[0])
+                        writer.writerow(list2)
+except Exception as error:
+    with open('error_file.txt', "a", encoding='utf-8-sig') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(error)
