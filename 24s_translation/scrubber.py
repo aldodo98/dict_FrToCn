89e0b209b41dd826979ab51a3b5a95b87153bd07
zhxu <aldo4611034@gmail.com>
2022-08-05 17:20:44 +0200

    测试

--- 24s_translation/scrubber.py
+++ 24s_translation/scrubber.py
@@ -74,9 +74,10 @@ def regroup_word_by_len(list):
         pass
 
 list_docs = ['enfant-f_accesoires.json','enfant-f_chaussures.json','enfant-f_offre.json','enfant-f_vetements.json','enfant-g_accesoirs.json','enfant-g_chaussures.json','enfant-g_offre.json','enfant-g_vetements.json','femme_accesoires.json','femme_beaute.json','femme_bijoux.json','femme_chaussures.json','femme_offre.json','femme_sac.json','femme_vetements.json','homme_accesoires.json','homme_beaute.json','homme_chaussures.json','homme_offre.json','homme_sac.json','homme_sport.json','homme_vetements.json','maison_arts.json','maison_deco.json','maison_linge.json','maison_luminaires.json','maison_mobilier.json','maison_offre.json']
-try:
 
-    for data_original in list_docs:
+for data_original in list_docs:
+    try:
+
         # file_path = 'D:\\dict_FrToCn\\24s_translation\\items_collection\\items_collection\\' + data_original
         file_path = '/root/dict_FrToCn/24s_translation/items_collection/items_collection/' + data_original
         with open(file_path,encoding='utf-8') as data_site:
@@ -139,6 +140,6 @@ try:
                         list2.append(each[1])
                         list2.append(each[0])
                         writer.writerow(list2)
-except Exception as error:
-    print(error)
-    pass
+    except Exception as error:
+        print(error)
+        pass
